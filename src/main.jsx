116c6f38f49894e8a79b3ca1982cd9886b0e5f88
habib-hb <developerhabib1230@gmail.com>
2024-04-24 12:12:52 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "https://cdn.skypack.dev/react@17.0.1";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.render(<App />, document.getElementById("root"));
